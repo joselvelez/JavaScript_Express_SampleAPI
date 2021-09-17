7987c206df1235de48843f9c89504fa0463f076f
joselvelez <joselvelez@protonmail.com>
2021-09-16 21:31:38 -0500

    added patch method

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { v4 as uuidv4, v4 } from 'uuid';
 
-const users = [];
+let users = [];
 
 // initialize Router
 const router = express.Router();
@@ -15,15 +15,35 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const user = req.body;
     users.push({...user, userId: v4()});
-    console.log(`POST request successful.`);
-    res.send('POST successful')
+    console.log(`POST request successful. Added ${user.firstName}`);
+    res.send(`POST request successful. Added ${user.firstName}`)
 })
 
-// get specific user by userID
+// get specific user by userId
 router.get('/:userId', (req, res) => {
     const { userId } = req.params;
     const userFound = users.find((user) => user.userId === userId);
     res.send(userFound)
 })
+
+// delete a specific userf by userId
+router.delete('/:userId', (req, res) => {
+    const { userId } = req.params;
+    users = users.filter((user) => user.userId != userId);
+
+    res.send(`deleting ${userId}`);
+})
+
+router.patch('/:userId', (req, res) => {
+    const { userId } = req.params;
+    const { firstName, lastName, age } = req.body;
+    const userToBeUpdated = users.find((user) => user.userId === userId);
+
+    if (firstName) userToBeUpdated.firstName = firstName;
+    if (lastName) userToBeUpdated.lastName = lastName;
+    if (age) userToBeUpdated.age = age;
+
+    res.send(`successfully updated userid ${userId}`);
+});
 
 export default router;
